1bbdb96051d8df3b9c2aa411d812faa91cf23ea5
emadurandal <emadurandal@gmail.com>
2020-02-05 16:06:33 +0900

    Refactor to use shaderity

--- index.js
+++ index.js
@@ -1,76 +1,6 @@
 const loaderUtils = require('loader-utils');
-const fs = require('fs');
-const path = require('path');
-
-
-function isVertexShader(path) {
-  const ext = fileExtension(path);
-  if (ext == 'vs' || ext == 'vert') {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function isFragmentShader(path) {
-  const ext = fileExtension(path);
-  if (ext == 'fs' || ext == 'ps' || ext == 'frag') {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function fileExtension(path) {
-  const splitted = path.split('.');
-  const ext = splitted[splitted.length - 1];
-
-  return ext;
-}
-
-function convertIn(str, loader) {
-  const inReg = /([\s\S]*)in /;
-  let inAsES1 = 'attribute ';
-  if (isFragmentShader(loader.resourcePath)) {
-    inAsES1 = 'varying ';
-  }
-  return str.replace(inReg, '$1' + inAsES1);
-}
-
-function transform(str, loader) {
-  str = convertIn(str, loader);
-  return str;
-}
-
-function shaderStage(loader) {
-  if (isVertexShader(loader.resourcePath)) {
-    return 'vertex';
-  } else {
-    return 'fragment';
-  }
-}
-
-function requireFile(source, resourcePath) {
-  const basePath = path.dirname(resourcePath) + '/';
-  const arr = source.split(/\r\n|\n/);
-
-  const newArr = [];
-  for (let i = 0; i < arr.length; i++){
-    const row = arr[i];
-    const match = row.match(/^(?![\/])[\t ]*#pragma[\t ]+shaderity:[\t ]*(\S*)[\t ]*=?[\t ]*require\([\t ]*(\S+)[\t ]*\)/);
-    if (match != null) {
-      const filePath = path.resolve(basePath + match[2]);
-      let extShader = fs.readFileSync(filePath, {encoding: 'utf-8'});
-      newArr.push(extShader);
-    } else {
-      newArr.push(row);
-    }
-  }
-
-  const requredShaderText = newArr.join('\n');
-
-  return requredShaderText;
-}
+const requireFile = require("shaderity").requireFile;
+const shaderStage = require("shaderity").shaderStage;
 
 module.exports = function(source, map, meta) {
   this.cacheable();
@@ -81,7 +11,7 @@ module.exports = function(source, map, meta) {
 
   json.code = requireFile(source, this.resourcePath);
 
-  json.shaderStage = shaderStage(this);
+  json.shaderStage = shaderStage(this.resourcePath);
 
   return `export default ${JSON.stringify(json)}`;
 };
